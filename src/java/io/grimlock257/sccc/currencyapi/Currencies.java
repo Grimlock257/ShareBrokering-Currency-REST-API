e08a2c6b7028afae7ea59246763616d94cea379b
Adam Watson <adamwatsonoa@gmail.com>
2020-11-29 14:34:25 +0000

    [SCCC-43] Initial commit of bare RESTful web service

---
+++ src/java/io/grimlock257/sccc/currencyapi/Currencies.java
@@ -0,0 +1,23 @@
+package io.grimlock257.sccc.currencyapi;
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * Logo
+ *
+ * Currencies path of the API, will request currencies from an external RestAPI
+ *
+ * @author Adam Watson
+ */
+@Path("currencies")
+public class Currencies {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getJson() {
+        return "{ \"success\": true }";
+    }
+}
