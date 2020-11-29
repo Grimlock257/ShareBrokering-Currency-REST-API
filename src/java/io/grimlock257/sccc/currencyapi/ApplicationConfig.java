9b5db612088f770acfd85241f1f190fe5d84f16f
Adam Watson <adamwatsonoa@gmail.com>
2020-11-29 14:37:15 +0000

    [SCCC-44] Move Currencies.java to /paths package

--- src/java/io/grimlock257/sccc/currencyapi/ApplicationConfig.java
+++ src/java/io/grimlock257/sccc/currencyapi/ApplicationConfig.java
@@ -25,7 +25,7 @@ public class ApplicationConfig extends Application {
      * Do not modify addRestResourceClasses() method. It is automatically populated with all resources defined in the project. If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(io.grimlock257.sccc.currencyapi.Currencies.class);
+        resources.add(io.grimlock257.sccc.currencyapi.paths.Currencies.class);
     }
 
 }
