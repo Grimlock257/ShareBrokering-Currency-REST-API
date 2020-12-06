b88bc44e90987c7f96fe5d67802fb0e2acb05fd1
Adam Watson <adamwatsonoa@gmail.com>
2020-12-06 19:35:17 +0000

    [SCCC-47] Cache conversation rates

---
+++ src/java/io/grimlock257/sccc/currencyapi/jobs/ExchangeRateJob.java
@@ -0,0 +1,159 @@
+package io.grimlock257.sccc.currencyapi.jobs;
+
+import com.google.gson.Gson;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.TreeMap;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
+
+/**
+ * ExchangeRateJob
+ *
+ * Singleton class to handle exchange rate updater task
+ *
+ * @author Adam Watson
+ */
+public class ExchangeRateJob {
+
+    private static ExchangeRateJob instance = null;
+
+    private Timer exchangeRateUpdaterTimer;
+
+    private final int EXCHANGE_RATE_UPDATE_INITIAL_DELAY = 0;
+    private final int EXCHANGE_RATE_UPDATE_FREQUENCY = 60 * 60 * 1000;
+
+    /**
+     * ExchangeRateJob constructor
+     *
+     * Private to enforce singleton behaviour
+     */
+    private ExchangeRateJob() {
+        setupCurrenciesUpdaterTask();
+    }
+
+    /**
+     * Initiate the currencies job if not already
+     */
+    public static void initiate() {
+
+        if (instance == null) {
+            instance = new ExchangeRateJob();
+        }
+    }
+
+    /**
+     * Get the instance of the ExchangeRateJob singleton
+     *
+     * @return The instance of the ExchangeRateJob
+     */
+    public static ExchangeRateJob getInstance() {
+
+        if (instance == null) {
+            instance = new ExchangeRateJob();
+        }
+
+        return instance;
+    }
+
+    /**
+     * Set up exchange rate updater task to run every EXCHANGE_RATE_UPDATE_FREQUENCY (in seconds) to retrieve latest exchange rates from the remote web service
+     */
+    private void setupCurrenciesUpdaterTask() {
+        exchangeRateUpdaterTimer = new Timer();
+
+        exchangeRateUpdaterTimer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                System.out.println("[CurrencyAPI] Updating exchange rates list...");
+
+                String exchangeRates = requestExchangeRate();
+
+                if (exchangeRates != null) {
+                    try (Writer fileWriter = new FileWriter("exchangeRates.json")) {
+                        fileWriter.write(exchangeRates);
+
+                        System.out.println("[CurrencyAPI] Exchange rate list update successful");
+                    } catch (IOException e) {
+                        System.err.println("[CurrencyAPI] IO exception writing exchangeRates.json: " + e.getMessage());
+                    }
+                } else {
+                    System.err.println("[CurrencyAPI] Error retrieving updated exchange rates");
+                }
+            }
+        }, EXCHANGE_RATE_UPDATE_INITIAL_DELAY, EXCHANGE_RATE_UPDATE_FREQUENCY);
+    }
+
+    /**
+     * Forcefully cancel the timer task
+     */
+    public void cancel() {
+        exchangeRateUpdaterTimer.cancel();
+        exchangeRateUpdaterTimer.purge();
+        exchangeRateUpdaterTimer = null;
+    }
+
+    /**
+     * Request exchange rates from the free currency API (has a base of USD)
+     *
+     * @return HashMap of currency code to rate against USD
+     */
+    private String requestExchangeRate() {
+        try {
+            // Request components
+            String baseUrl = "https://openexchangerates.org/api/latest.json";
+            String apiKey = "your-api-key";
+            String apiQueryParam = "?app_id=" + apiKey;
+
+            // Create URL object
+            URL url = new URL(baseUrl + apiQueryParam);
+
+            // Create HTTP connection
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+
+            // If the response was not a 200, throw an error
+            if (conn.getResponseCode() != 200) {
+                throw new IOException(conn.getResponseMessage());
+            }
+
+            // Retrieve the connection input stream and store as a JsonObject
+            JsonReader jsonReader = Json.createReader(conn.getInputStream());
+            JsonObject jsonObject = jsonReader.readObject();
+            JsonObject jsonExchangeRates = jsonObject.getJsonObject("rates");
+
+            // TreeMap<String, String> to store currencyCode : currency name in an ordered format
+            Map<String, Double> exchangeRateMap = new TreeMap<>();
+
+            // Iterate over the resutls object, and add the key and currencyName field to the currencyMap
+            for (String key : jsonExchangeRates.keySet()) {
+                double value = jsonExchangeRates.getJsonNumber(key).doubleValue();
+
+                exchangeRateMap.put(key, value);
+            }
+
+            // Using GSON to convert our Map<String, Double> to a JSON representation
+            Gson gson = new Gson();
+            String exchangeRateMapJson = gson.toJson(exchangeRateMap);
+
+            return exchangeRateMapJson;
+        } catch (MalformedURLException e) {
+            System.err.println("[CurrencyAPI] Malformed URL: " + e.getMessage());
+        } catch (IOException e) {
+            System.err.println("[CurrencyAPI] IOException connecting to URL: " + e.getMessage());
+        } catch (NullPointerException e) {
+            System.err.println("[CurrencyAPI] NPE: " + e.getMessage());
+        } catch (ClassCastException e) {
+            System.err.println("[CurrencyAPI] ClassCastException (rates is likely null): " + e.getMessage());
+        }
+
+        return null;
+    }
+}
